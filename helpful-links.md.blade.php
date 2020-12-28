18c0ca0025c93c90604fd66a4aa8a784d4d1a8f6
David VanScott <david.vanscott@gmail.com>
2020-12-28 11:21:21 -0500

    Minor fixes

--- helpful-links.md.blade.php
+++ helpful-links.md.blade.php
@@ -4,17 +4,17 @@
 
 - [CodeIgniter Documentation](https://codeigniter.com/userguide2/index.html)
 
-## PHP
+## Learn PHP
 
 Nova is built in PHP, a widely used server-side programming language that’s become increasingly fast and powerful over the years. PHP works well with HTML and databases, making it a great language for building dynamic web applications. If you want to tinker with Nova or even create your own extensions, learning PHP is crucial. These resources will get you on your way.
 
-- [Laracasts PHP for Beginners](https://laracasts.com/series/php-for-beginners)
-- [Codecademy Learn PHP](https://www.codecademy.com/learn/learn-php)
+- [Laracasts - PHP for Beginners](https://laracasts.com/series/php-for-beginners)
+- [Codecademy - Learn PHP](https://www.codecademy.com/learn/learn-php)
 - [Learn PHP](https://www.learn-php.org/)
 
-## CSS
+## Learn CSS
 
 All of Nova's themes rely on a heavy dose of Cascading Style Sheets (CSS) to do all of the styling. If you want to change styling for a theme or create your own theme, learning how to write CSS is crucial. These resources will get you on your way.
 
+- [Codecademy - Learn CSS](https://www.codecademy.com/learn/learn-css)
 - [How to learn CSS](https://www.smashingmagazine.com/2019/01/how-to-learn-css/)
-- [Codecademy Learn CSS](https://www.codecademy.com/learn/learn-css)
