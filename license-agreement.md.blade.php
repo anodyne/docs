c7a44d02a264f8cf531a1effe20cf920462c7a6e
David VanScott <david.vanscott@gmail.com>
2020-12-28 11:18:23 -0500

    Add helpful links and move license

--- license-agreement.md.blade.php
+++ license-agreement.md.blade.php
@@ -8,6 +8,7 @@ Copyright (c) 2010-2021, Anodyne Productions
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
 - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 - Neither the names of Nova, Anodyne, SMS nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
