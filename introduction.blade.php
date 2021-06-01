d320877804348ecc70548f8f7018e3248f101059
David VanScott <david.vanscott@gmail.com>
2021-05-31 22:43:46 -0400

    Wip

--- introduction.blade.php
+++ introduction.blade.php
@@ -1,8 +1,8 @@
 <x-docs-layout :sections="$sections" :current="$page" :version="$version">
   <div class="docs prose prose-lg prose-amber">
     <h1>Getting started with Nova</h1>
-
     <p>Learn Nova the way that best matches your learning style.</p>
+    <hr />
 
     <h2>What's new in Nova</h2>
 
